32fbea8ff357ee809f4338e3ff749e1484cce259
Khilan10 <mahajankhilan1@gmail.com>
2020-02-13 18:01:49 +0530

    - Added Header  in the common folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Header from '../src/common/header/Header'
 
-ReactDOM.render(<span>
-    Image Viewer
-</span>, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
