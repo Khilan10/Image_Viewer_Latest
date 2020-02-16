7ba05f4201a498faed3714735d14a9760c8265b1
Khilan10 <mahajankhilan1@gmail.com>
2020-02-16 22:16:09 +0530

    - Added code for  search and add comment functionality

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -15,6 +15,7 @@ import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import Button from '@material-ui/core/Button';
 
 class Home extends Component {
@@ -24,8 +25,13 @@ class Home extends Component {
         this.state = {
             data: [],
             postData: [],
+            postData2: [],
             like: [],
-            liked: []
+            liked: [],
+            isCommentAdded: [],
+            commentAdded: [],
+            comment: [],
+            commentRequired: []
         }
     }
 
@@ -51,22 +57,45 @@ class Home extends Component {
         xhrPostData.addEventListener("readystatechange", function () {
             if (this.readyState === 4) {
                 that.setState({
-                    postData: JSON.parse(this.responseText)
+                    postData: JSON.parse(this.responseText),
+                    postData2: JSON.parse(this.responseText)
                 })
                 let length = that.state.postData.data.length;
                 let initialLike = []
+                let initialLiked = []
+                let isCommentAddedInitial = []
+                let commentInitial = []
+                let commentRequiredInitial = []
+                let commentAddedInitial = []
                 for (var i = 0; i < length; i++) {
                     initialLike.push("dispBlock");
+                    initialLiked.push("dispNone");
+                    isCommentAddedInitial.push("dispNone");
+                    commentInitial.push('');
+                    commentRequiredInitial.push("dispNone")
+                    commentAddedInitial.push('');
                 }
                 that.setState({
                     like: initialLike
                 })
-                let initialLiked = []
-                for (var j = 0; j < length; j++) {
-                    initialLiked.push("dispNone");
-                }
+
                 that.setState({
                     liked: initialLiked
+                })
+                that.setState({
+                    isCommentAdded: isCommentAddedInitial
+                })
+                that.setState({
+                    commentAdded: commentAddedInitial
+                })
+                that.setState({
+                    comment: commentInitial
+                })
+                that.setState({
+                    commentRequired: commentRequiredInitial
+                })
+                that.setState({
+                    postDataSearch: postData
                 })
                 console.log("checking post data" + JSON.stringify(that.state.postData.data[0].user.id))
                 console.log("checking post data" + JSON.stringify(that.state.postData))
@@ -75,6 +104,18 @@ class Home extends Component {
         xhrPostData.open("GET", "https://api.instagram.com/v1/users/self/media/recent?access_token=8661035776.d0fcd39.39f63ab2f88d4f9c92b0862729ee2784");
         xhrPostData.send(postData);
 
+    }
+
+    searchChangeHandler = (e) => {
+        if (e !== "") {
+            let searchPosts = this.state.postData;
+            let indSearchPosts = searchPosts.data;
+            let indSearchPostsResults = indSearchPosts.filter(indSearchPost => indSearchPost.caption.text.includes(e))
+            searchPosts.data = indSearchPostsResults;
+            this.setState({ postData: searchPosts });
+        } else {
+            this.componentWillMount();
+        }
     }
 
     likeClickHandler = (index) => {
@@ -102,6 +143,38 @@ class Home extends Component {
         this.setState({ liked: updateLiked })
     }
 
+    inputCommentChangeHandler = (e, index) => {
+        let commentAddedUpdate = this.state.commentAdded;
+        commentAddedUpdate[index] = e;
+        this.setState({ commentAdded: commentAddedUpdate });
+    }
+
+    addCommentClickHandler = (index) => {
+        console.log("add commenthandler called for :" + index)
+        if (this.state.commentAdded[index] === '' || this.state.commentAdded[index] === null) {
+            let commentRequiredUpdate = this.state.commentRequired;
+            commentRequiredUpdate[index] = "dispBlock";
+            this.setState({ commentRequired: commentRequiredUpdate });
+        }
+        else {
+            let length = this.state.postData.data.length;
+            let commentRequiredUpdate = this.state.commentRequired;
+            let isCommentAddedUpdate = this.state.isCommentAdded;
+            let commentUpdate = this.state.commentAdded;
+            let commentAddedUpdate = this.state.commentAdded;
+            commentRequiredUpdate[index] = "dispNone";
+            isCommentAddedUpdate[index] = "dispBlock"
+            this.setState({ commentRequired: commentRequiredUpdate });
+            this.setState({ isCommentAdded: isCommentAddedUpdate });
+            this.setState({ comment: commentUpdate })
+            // for (var i = 0; i < length; i++) {
+            //     commentAddedUpdate.push('');
+            // }
+            // this.setState({ commentAdded: commentAddedUpdate });
+        }
+
+    }
+
     render() {
         let pdata = this.state.postData;
         return (
@@ -113,7 +186,9 @@ class Home extends Component {
                     <div className="header-right-content">
                         <div className="search-box">
                             <SearchIcon className="search-icon" />
-                            <Input type="text" id="search" placeholder="Search..." disableUnderline className="input-box" />
+                            <Input type="text" id="search" placeholder="Search..." onChange={(e) => {
+                                this.searchChangeHandler(e.target.value)
+                            }} disableUnderline className="input-box" />
                         </div>
                         <div className="icon-button">
                             <IconButton className="circle"><img src={this.state.data.profile_picture} alt={this.state.data.username} height="36" width="36" className="circle" /></IconButton>
@@ -146,6 +221,7 @@ class Home extends Component {
 
                                             ))}
                                         </Typography>
+                                        <br />
                                         <div className="likes">
                                             <span className={this.state.like[index]}>
                                                 <FavoriteBorderIcon id={"notlike" + data.caption.id} onClick={() => this.likeClickHandler(index)} />
@@ -155,14 +231,25 @@ class Home extends Component {
                                             </span>
                                             <span className="like-count">{data.likes.count} likes</span>
                                         </div>
+                                        <br />
+                                        <div className={this.state.isCommentAdded[index]}>
+                                            <span className="bold">{this.state.data.username} : </span>
+                                            {this.state.comment[index]}
+                                        </div>
                                         <br /><br />
                                         <FormControl>
                                             <span >
                                                 <InputLabel htmlFor="comment" className="lable">Add a comment</InputLabel>
                                                 <Input id={"comment" + index}
-                                                    type="text" className="lable" />
-                                                <Button variant="contained" color="primary"><Typography variant="subtitle1">ADD</Typography></Button>
+                                                    type="text" className="lable" onChange={(e) => this.inputCommentChangeHandler(e.target.value, index)} />
+                                                <Button variant="contained" color="primary"><Typography variant="subtitle1" onClick={() => this.addCommentClickHandler(index)}>ADD</Typography></Button>
                                             </span>
+                                        </FormControl>
+                                        <FormControl>
+                                            <FormHelperText className={this.state.commentRequired[index]}>
+                                                <span className="red">Please add a comment</span>
+                                                <br />
+                                            </FormHelperText>
                                         </FormControl>
                                     </CardContent>
                                 </Card>
